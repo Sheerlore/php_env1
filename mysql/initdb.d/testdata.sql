b9bf7635c585d860d05a7948447fd9b6d4fe416d
uai <noema01@outlook.jp>
2020-09-22 21:43:22 +0900

    small change, delete sqlMyAdmin

---
+++ mysql/initdb.d/testdata.sql
@@ -0,0 +1,6 @@
+INSERT INTO test_comments (id, name, text) 
+VALUES (1, 'AIUEO', 'aiueo'),
+(2, 'KAKIKUKEKO', 'kakikukeko'),
+(3, 'SASISUSESO', 'sasisuseso'),
+(4, 'TATITUTETO', 'tatituteto'),
+(5, 'NANINUNENO', 'naninuneno');
