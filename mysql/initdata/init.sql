d729db7bc483fef099bbe278b87a9868bc4545a8
uai <noema01@outlook.jp>
2020-09-24 11:22:10 +0900

    modify mysql

---
+++ mysql/initdata/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE sample_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    nama VARCHAR(20) NOT NULL,
+    comment VARCHAR(140) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO sample_table 
+(name, comment)
+VALUES
+    ('テストName1', 'テストcomment1'),
+    ('テストName2', 'テストcomment2'),
+    ('テストName3', 'テストcomment3'),
+    ('テストName4', 'テストcomment4'),
+    ('テストName5', 'テストcomment5'),
+    ('テストName6', 'テストcomment6');
--- mysql/initdb.d/testdata.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO test_comments (id, name, text) 
-VALUES (1, 'AIUEO', 'aiueo'),
-(2, 'KAKIKUKEKO', 'kakikukeko'),
-(3, 'SASISUSESO', 'sasisuseso'),
-(4, 'TATITUTETO', 'tatituteto'),
-(5, 'NANINUNENO', 'naninuneno');
